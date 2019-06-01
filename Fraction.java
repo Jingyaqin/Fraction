846f086ba9572715d389231010e2a9e90ccb0a83
Jingyaqin <50492211+Jingyaqin@users.noreply.github.com>
2019-06-01 09:32:25 -0400

    Add files via upload

--- Fraction.java
+++ Fraction.java
@@ -1,28 +1,28 @@
 import java.io.*;
 public class Fraction{
-    private int numerator;
-    private int denominator;
-
-    public String toString(){
-        int n = numerator;
-        int d = denominator;
-        System.out.println(n/d);
-    }
-public Fraction(int numberator, int denominator){ 
-if(denominator == 0){
-    throw new IllegalArgumentException("denominator is zero");
+  private int numerator;
+  private int denominator;
+  public String toString(){
+   int n = numerator;
+   int d = denominator;
+   System.out.println(n/d);
+  }
 }
-if(denominator < 0) {
+public Fraction(int numberator, int denominator){ 
+  if(denominator == 0){
+    throw new IllegalArgumentException("denominator is zero");
+  }
+  if(denominator < 0) {
         numerator *= -1;
         denominator *= -1;
-}
+  }
 }
 public Fraction(int wholeNumber){
-wholeNumber = numerator;
-denominator = 1;
+  numerator = wholeNumber;
+  denominator = 1;
 }
 public Fraction(String fraction){
-    String fraction = new String();
-    fraction = fraction.trim();
-    int fraction = Integer.parseInt(String);
+  String fraction = new String();
+  fraction = fraction.trim();
+  int fraction = Integer.parseInt();
 }
